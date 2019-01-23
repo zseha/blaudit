1ad2da7b42d11192213396de3c3052b5b6ec1a2f
Zlatko Sehanovic <zs@ilvi.io>
2019-01-23 11:29:14 +0100

    init.

--- app/src/main/java/com/zseha/blaudit/MainActivity.java
+++ app/src/main/java/com/zseha/blaudit/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import org.qpython.qsl4a.*;
+import org.qpython.qsl4a.qsl4a.activity.FutureActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this,QSL4APP.class);
+        Intent intent = new Intent(this, FutureActivity.class);
         startActivity(intent);
     }
 }
